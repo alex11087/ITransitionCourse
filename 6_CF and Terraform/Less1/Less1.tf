19e7a8ef152082b0e03b0e90fafbb17cfab91bd0
Alexey Bobrov <alexxxeius@gmail.com>
2021-05-04 12:57:48 +0300

    Commit

--- 6_CF and Terraform/Less1/Less1.tf
+++ 6_CF and Terraform/Less1/Less1.tf
@@ -1,11 +1,9 @@
 provider "aws" {
-  access_key = "AKIAY4JNO7BXVC6P4GMZ"
-  secret_key = "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"
-  region = "eu-central-1"
+  region = "us-east-2"
 }
 
 
-resource "aws_instance" "myUbuntu" {
-    ami = "ami-05f7491af5eef733a"
-    instance_type = "t2.micro"
+resource "aws_instance" "my_Ubuntu" {
+  ami           = "ami-00399ec92321828f5"
+  instance_type = "t2.micro"
 }
